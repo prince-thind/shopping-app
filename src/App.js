4bc5c5cdf6cbb8ebd39d8a80beaa051d8b639bd8
Prince Thind <princethind799@gmail.com>
2021-06-30 14:25:44 +0530

    implement basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import About from './components/About.js';
+import Shop from './components/Shop.js';
+import Cart from './components/Cart.js';
 
 function App() {
+  const [items, setItems] = useState(0);
+
+  function itemSub(signal) {
+    if (signal === 'decrement') {
+      setItems(items - 1);
+    } else if (signal === 'increment') {
+      setItems(items + 1);
+    } else if (signal === 'reset') {
+      setItems(0);
+    }
+  }
+
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="nav-bar">
+        <Link to="/">Home</Link>
+          <ul className="nav-bar-list">
+            <Link to="/about">About</Link>
+            <Link to="/shop">Shop</Link>
+            <Link to="/cart">Cart ({items})</Link>
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+        <Route path="/about" exact>
+         <About/>
+        </Route>
+        <Route path="/shop" exact>
+         <Shop itemSub={itemSub}/>
+        </Route>
+        <Route path="/cart" exact>
+         <Cart itemSub={itemSub}/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,5 @@
+function About(){
+    return <h1>hello this is About</h1>
+}
+
+export default About;
