6884f4cb1a1481ba47b3783b2d67cda5ece26262
Prince Thind <princethind799@gmail.com>
2021-06-30 18:43:24 +0530

    add images.js for exporting images

---
+++ src/images/images.js
@@ -0,0 +1,10 @@
+import cup from './cup.jpg';
+import feather from './feather.jpg';
+import griffin from './griffin.jpg';
+import home from './home.jpg';
+import jar from './jar.jpg';
+import rings from './rings.jpg';
+import watch from './watch.jpg';
+
+const images = {cup, feather, griffin, home, jar, rings, watch};
+export default images;
