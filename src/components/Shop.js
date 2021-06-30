4caa821466f84a84314bf28311f1300b880a4a38
Prince Thind <princethind799@gmail.com>
2021-06-30 20:06:01 +0530

    extract items as its own component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,14 +1,18 @@
-import Item from './Item.js'
+import Item from './Item.js';
 
 function Shop(props) {
   return (
     <div className="shop-content">
       <h2> Items Available:</h2>
-      {props.items.map((item) => {
-        return <Item key={item.key} item={item} images={props.images} cartSub={props.cartSub} />;
-      })}
+      <Items />
     </div>
   );
+
+  function Items() {
+    return <div>{props.items.map((item) => {
+      return <Item key={item.key} item={item} images={props.images} cartSub={props.cartSub} />;
+    })}</div>
+  }
 }
 
 export default Shop;
