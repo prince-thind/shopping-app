fc87a1db995f2b77d02fcadfaa77448a83a17dd3
Prince Thind <princethind799@gmail.com>
2021-06-30 21:46:04 +0530

    refactor jsx

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
-//import React from 'react';
+import '../styles/Home.css';
 
 function Home(props) {
-  return <div className="home-content">
-      <img src={props.img} className="home-image" alt="home"/>
-      <h2 className="home-heading"> Foo Shop: your first source for Buying most authentic Products</h2>
-  </div>
+  return (
+    <div className="home-content">
+      <h2 className="home-heading"> Foo Shop</h2>
+      <img src={props.img} className="home-image" alt="home" />
+      <p className="home-intro">Your first choice for products</p>
+    </div>
+  );
 }
 
 export default Home;
