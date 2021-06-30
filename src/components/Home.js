a4564017665f6f757fb445e74e113c053a6fe3d4
Prince Thind <princethind799@gmail.com>
2021-06-30 18:42:50 +0530

    create basic home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>hello this is Home</h1>
+//import React from 'react';
+
+function Home(props) {
+  return <div className="home-content">
+      <img src={props.img} className="home-image image" alt="home"/>
+      <h2 className="home-heading"> Foo-Bar Shop: your first source for Buying most authentic Products</h2>
+  </div>
 }
 
 export default Home;
