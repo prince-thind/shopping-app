21cba275a545cd71b35e4690c785b221085fe0ec
Prince Thind <Princethind799@gmail.com>
2021-12-03 18:11:39 +0530

    improve homepage styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
-import '../styles/Home.css';
+import "../styles/Home.css";
 
 function Home(props) {
   return (
     <div className="home-content">
       <h2 className="home-heading"> Foo Shop</h2>
-      <img src={props.img} className="home-image" alt="home" />
+      <div className="home-image-container">
+        <img src={props.img} className="home-image" alt="home" />
+      </div>
       <p className="home-intro">Your first choice for products</p>
     </div>
   );
