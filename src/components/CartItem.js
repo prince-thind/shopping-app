de5e9a0f6d480082388635d242fd8d732afb58a1
Prince Thind <Princethind799@gmail.com>
2021-12-05 01:21:02 +0530

    update cart page

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,39 +1,42 @@
-import '../styles/CartItem.css';
+import "../styles/CartItem.css";
 
 function CartItem({ item, cartSub }) {
   return (
     <div className="cart-item">
-      <div className="cart-item-name">Name: {item.name}</div>
-      <div className="cart-item-count">
-        Count: {item.count} ({item.currency}
-        {+item.price * +item.count})
+      <div className="cart-item-image-container">
+        <img src={item.image} alt="cart item" className="cart-item-image" />
       </div>
-      <div className="cart-item-buttons">
-        <button className="cart-add-button button" onClick={increaseCount}>
-          Add More
-        </button>
-        <RemoveButton />
+      <div className="cart-item-content">
+        <div className="cart-item-name">{item.name}</div>
+        <div className="cart-item-price">
+       {item.price}
+          {item.currency}
+        </div>
+      </div>
+
+      <div className="cart-capacity">
+        <div className="cart-item-buttons">
+          <button className="cart-remove-button button" onClick={decreaseCount}>
+            -
+          </button>
+          <div className="cart-item-count">{item.count}</div>
+          <button className="cart-add-button button" onClick={increaseCount}>
+            +
+          </button>
+        </div>
+        <div className="cart-price">
+          ({item.currency}
+          {+item.price * +item.count})
+        </div>
       </div>
     </div>
   );
 
   function increaseCount() {
-    cartSub('increment', item.key);
+    cartSub("increment", item.key);
   }
   function decreaseCount() {
-    cartSub('decrement', item.key);
-  }
-
-  function RemoveButton({ count }) {
-    if (count === 0) {
-      return null;
-    }
-
-    return (
-      <button className="cart-remove-button button" onClick={decreaseCount}>
-        remove
-      </button>
-    );
+    cartSub("decrement", item.key);
   }
 }
 
