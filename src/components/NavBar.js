12f1bdf061afe49bffc69a5683e1995f63deb469
Prince Thind <Princethind799@gmail.com>
2021-12-05 12:54:41 +0530

    implement active page highlight

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,67 @@
 import { Link } from "react-router-dom";
-import "../styles/Navbar.css"
+import React, { useState } from "react";
 
-function Navbar({count}) {
+import "../styles/Navbar.css";
+
+function Navbar({ count }) {
+  const [activePage, setActivePage] = useState("home");
+
   return (
     <nav className="nav-bar">
-      <Link to="/">
-        <span>Home</span>
-      </Link>
+      <Page
+        activePage={activePage}
+        currentPage="home"
+        setActivePage={setActivePage}
+      >
+        <Link to="/">Home</Link>
+      </Page>
       <ul className="nav-bar-list">
         <li>
-          <Link to="/about">About</Link>
+          <Page
+            activePage={activePage}
+            currentPage="about"
+            setActivePage={setActivePage}
+          >
+            <Link to="/about">About</Link>
+          </Page>
         </li>
         <li>
-          <Link to="/shop">Shop</Link>
+          <Page
+            activePage={activePage}
+            currentPage="shop"
+            setActivePage={setActivePage}
+          >
+            <Link to="/shop">Shop</Link>
+          </Page>
         </li>
         <li>
-          <Link to="/cart">
-            Cart(
-            {count}
-            )
-          </Link>
+          <Page
+            activePage={activePage}
+            currentPage="cart"
+            setActivePage={setActivePage}
+          >
+            <Link to="/cart">
+              Cart(
+              {count})
+            </Link>
+          </Page>
         </li>
       </ul>
     </nav>
   );
+}
+
+function Page({ activePage, currentPage, setActivePage, children }) {
+  const pageStatus = activePage === currentPage ? "active-page" : "";
+  return (
+    <div onClick={handleClick} className={pageStatus}>
+      {children}
+    </div>
+  );
+
+  function handleClick() {
+    setActivePage(currentPage);
+  }
 }
 
 export default Navbar;
