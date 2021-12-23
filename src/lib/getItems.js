16cb16ed281a16a3d5bcf9561936f19d62474eaf
Prince Thind <Princethind799@gmail.com>
2021-12-23 20:36:41 +0530

    add fakerjs to getitems functions

--- src/lib/getItems.js
+++ src/lib/getItems.js
@@ -1,57 +1,28 @@
 import uniqid from "uniqid";
-import images from "../images/images";
+import faker from "faker";
 
 async function getItems() {
-  return [
-    {
-      name: "cup",
-      key: uniqid(),
-      price: "5",
-      count: 0,
-      currency: "$",
-      image: images["cup"],
-    },
-    {
-      name: "feather",
-      price: "10",
-      key: uniqid(),
-      count: 0,
-      currency: "$",
-      image: images["feather"],
-    },
-    {
-      name: "griffin",
-      key: uniqid(),
-      price: "50",
-      count: 0,
-      currency: "$",
-      image: images["griffin"],
-    },
-    {
-      name: "jar",
-      key: uniqid(),
-      price: "5",
-      count: 0,
-      currency: "$",
-      image: images["jar"],
-    },
-    {
-      name: "rings",
-      key: uniqid(),
-      price: "75",
-      count: 0,
-      currency: "$",
-      image: images["rings"],
-    },
-    {
-      name: "watch",
-      key: uniqid(),
-      price: "100",
-      count: 0,
-      currency: "$",
-      image: images["watch"],
-    },
-  ];
+  const items = [];
+  const numberOfItems = Math.trunc(Math.random() * 10 + 5);
+
+  for (let i = 0; i < numberOfItems; i++) {
+    const item = generateItem();
+    console.log(item);
+    items.push(item);
+  }
+
+  return items;
+
+  function generateItem() {
+    const item = {};
+    item.name = faker.commerce.productName();
+    item.key = uniqid();
+    item.price = faker.commerce.price(100, 200);
+    item.count = 0;
+    item.currency = "$";
+    item.image = faker.image.image(null, null, true);
+    return item;
+  }
 }
 
 export default getItems;
