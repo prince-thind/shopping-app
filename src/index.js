eb78b6275a5130d5561f7619bf82e7653c2e25e3
Prince Thind <Princethind799@gmail.com>
2022-02-01 16:32:46 +0530

    setup basic redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
