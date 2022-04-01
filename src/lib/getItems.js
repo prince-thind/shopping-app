34e9aabeaa7e9f7499ce88ecd59791242f9c41b5
Prince Thind <Princethind799@gmail.com>
2022-04-01 22:45:58 +0530

    update fetch URL

--- src/lib/getItems.js
+++ src/lib/getItems.js
@@ -1,5 +1,5 @@
-import { randAccessory, randUuid } from '@ngneat/falso';
-import axios from 'axios';
+import { randAccessory, randUuid } from "@ngneat/falso";
+import axios from "axios";
 
 async function getItems() {
   let result = [];
@@ -9,28 +9,28 @@ async function getItems() {
     result.push(item);
   }
 
-  result=await Promise.all(result);
+  result = await Promise.all(result);
   return result;
 
   async function generateItem() {
     const item = {};
     item.name = randAccessory();
     item.key = randUuid(); //todo
-    item.price = Math.trunc(Math.random()*(100)+50);
+    item.price = Math.trunc(Math.random() * 100 + 50);
     item.count = 0;
     item.currency = "$";
     item.image = await getImageSrc(item.name);
     return item;
   }
 
-  async function getImageSrc(name){
-    const URL=process.env.REACT_APP_FETCH_URL; //development
-    const response=await axios.get(URL,{
+  async function getImageSrc(name) {
+    const URL = `https://prince-shopping-app-backend.herokuapp.com/`; //development
+    const response = await axios.get(URL, {
       params: {
-        q:name.replaceAll(' ','+'),
+        q: name.replaceAll(" ", "+"),
       },
     });
-    const src=response.data;
+    const src = response.data;
     return src;
   }
 }
