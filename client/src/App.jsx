471249a11e8afa263ca473012ae1d1b7d557fd63
Prince Thind <princethind799@gmail.com>
2022-06-08 13:26:13 +0530

    setup basic page routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import About from "./pages/about/About";
+import Cart from "./pages/cart/Cart";
+import Home from "./pages/home/Home";
+import Item from "./pages/item/Item";
+import Items from "./pages/items/Items";
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="items" element={<Items />}>
+              <Route path=":itemID" element={<Item />} />
+            </Route>
+            <Route path="cart" element={<Cart />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
