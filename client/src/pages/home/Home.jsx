4c887e48ead7097fcada221c4d030b705a0f6708
Prince Thind <princethind799@gmail.com>
2022-06-09 16:31:35 +0530

    create homepage

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,3 +1,23 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import mainImage from "./assets/main.jpg";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Box sx={{mt:5}}>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Paper sx={{ width: "fit-content" }} elevation={7}>
+          <img src={mainImage} className="main-image" alt="shop"/>
+        </Paper>
+      </Box>
+      <Typography
+        component="h2"
+        variant="h4"
+        sx={{ display: "flex", justifyContent: "center", mt: 2 }}
+      >
+        Foo Shop, Your destination for all Foo content
+      </Typography>
+    </Box>
+  );
 }
