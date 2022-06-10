30c80fa80a8e66eaff5f0e614d32f60e1d52069a
Prince Thind <princethind799@gmail.com>
2022-06-10 12:39:28 +0530

    add mock items in shop

--- client/src/pages/items/Items.jsx
+++ client/src/pages/items/Items.jsx
@@ -1,3 +1,64 @@
+import Grid from "@mui/material/Grid";
+import ItemCard from "./components/ItemCard";
+
 export default function Items() {
-  return <div>Items</div>;
+  const data = fakeSampleData();
+
+  return (
+    <Grid container spacing={5} sx={{ mb: 10 }}>
+      {data.map((item, index) => {
+        item.id = index;
+        return (
+          <Grid item xs={3}>
+            <ItemCard item={item} />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+}
+
+function fakeSampleData() {
+  return [
+    {
+      name: "Item1",
+      price: 45,
+      count: 0,
+      imageSrc:
+        "https://cdn.pixabay.com/photo/2016/09/01/09/52/seagull-1635657_960_720.jpg",
+      shortDescription: "a very short shortDescription lorem ipsum",
+    },
+    {
+      name: "Item2",
+      price: 45,
+      count: 0,
+      imageSrc:
+        "https://cdn.pixabay.com/photo/2021/10/10/06/22/seagull-6695728_960_720.jpg",
+      shortDescription: "a very short shortDescription lorem ipsum",
+    },
+    {
+      name: "Item3",
+      price: 45,
+      shortDescription: "a very short shortDescription lorem ipsum",
+      count: 0,
+      imageSrc:
+        "https://cdn.pixabay.com/photo/2022/05/30/14/05/paw-7231300_960_720.jpg",
+    },
+    {
+      name: "Item4",
+      shortDescription: "a very short shortDescription lorem ipsum",
+      price: 45,
+      count: 0,
+      imageSrc:
+        "https://cdn.pixabay.com/photo/2022/01/22/09/42/animal-6956681_960_720.jpg",
+    },
+    {
+      name: "Item5",
+      price: 45,
+      shortDescription: "a very short shortDescription lorem ipsum",
+      count: 0,
+      imageSrc:
+        "https://cdn.pixabay.com/photo/2022/06/02/11/01/snowdrop-anemone-7237694_960_720.jpg",
+    },
+  ];
 }
---
+++ client/src/pages/items/components/ItemCard.jsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Rating from "@mui/material/Rating";
+import Box from "@mui/material/Box";
+import CustomLink from "../../../components/CustomLink";
+
+export default function ItemCard({ item }) {
+  return (
+    <CustomLink to={"/items/" + (item.id+1)}>
+      <Paper elevation={10} sx={{ p: 1, pb:3 }}>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <img src={item.imageSrc} className="item-image" alt="item" />
+        </Box>
+        <Box sx={{ p:2, pb:1 }}>
+          <Typography variant="h5" component="h3">
+            {item.name}
+          </Typography>
+          <Rating name="read-only" value={3} readOnly precision={0.5} />
+
+          <Typography
+            variant="body2"
+            component="h4"
+            sx={{ color: "text.secondary" }}
+          >
+            ${item.price}
+          </Typography>
+          <Typography
+            variant="body2"
+            component="p"
+            sx={{ color: "text.primary", mt: 1 }}
+          >
+            {item.shortDescription}
+          </Typography>
+        </Box>
+      </Paper>
+    </CustomLink>
+  );
+}
