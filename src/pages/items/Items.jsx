4ddb2a42ff1d0176c5ab4076f83e974e62e8dc31
Prince Thind <princethind799@gmail.com>
2022-06-12 14:53:04 +0530

    add pixabay contribution

--- src/pages/items/Items.jsx
+++ src/pages/items/Items.jsx
@@ -1,4 +1,7 @@
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 import ItemCard from "./components/ItemCard";
 import WaitingIcon from "./components/WaitingIcon";
 import { useSelector } from "react-redux";
@@ -13,14 +16,30 @@ export default function Items() {
   }
 
   return (
-    <Grid container spacing={5} sx={{ mb: 10 }}>
-      {data.map((item) => {
-        return (
-          <Grid item xs={3} key={item.id}>
-            <ItemCard item={item} />
-          </Grid>
-        );
-      })}
-    </Grid>
+    <Box>
+      <Grid container spacing={5} sx={{ mb: 10 }}>
+        {data.map((item) => {
+          return (
+            <Grid item xs={3} key={item.id}>
+              <ItemCard item={item} />
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Paper
+        sx={{ textAlign:'center', p:3, mb:5 }}
+        elevation={10}
+      >
+        <Typography variant="h6" component="h6">
+          All Images obtained from:
+        </Typography>
+        <a href="https://pixabay.com/" target="_blank" rel="noreferrer">
+          <img
+            src="https://pixabay.com/static/img/public/leaderboard_b.png"
+            alt="Pixabay"
+          />
+        </a>
+      </Paper>
+    </Box>
   );
 }
